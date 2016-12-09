8a963f418d83e9b0ce92c4ae807a262b5c0c8bf0
maning <maning1x@163.com>
2016-12-09 22:12:13 +0800

    build vim8.0 by source code in ubuntu 16.04

--- build_install_vim.sh
+++ build_install_vim.sh
@@ -62,6 +62,51 @@ function build_and_install_vim()
 	cat $vimcfig_bundle_dir_path/.self_mod/highlight_code.vim >> /usr/share/vim/vim74/syntax/c.vim
 }
 
+#build vim
+function build_vim_by_source()
+{
+
+	if which apt-get > /dev/null
+	then
+		sudo apt-get install -y ctags build-essential cmake python-dev python3-dev fontconfig git
+		var=$(sudo cat /etc/lsb-release | grep "DISTRIB_RELEASE")
+		systemVersion='DISTRIB_RELEASE=16.04'
+		if [ $var == $systemVersion ]
+		then
+			sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
+				libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+				libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+				python3-dev ruby-dev lua5.1 lua5.1-dev
+			sudo apt-get remove -y vim vim-runtime gvim
+			sudo apt-get remove -y vim-tiny vim-common vim-gui-common vim-nox
+
+			sudo rm -rf ~/vim
+			sudo rm -rf /usr/share/vim/vim74
+			sudo rm -rf /usr/share/vim/vim80
+
+			git clone https://github.com/vim/vim.git ~/vim
+			cd ~/vim
+			./configure --with-features=huge \
+				--enable-multibyte \
+				--enable-rubyinterp \
+				--enable-pythoninterp \
+				--with-python-config-dir=/usr/lib/python2.7/config \
+				--enable-perlinterp \
+				--enable-luainterp \
+				--enable-gui=gtk2 --enable-cscope --prefix=/usr
+			make VIMRUNTIMEDIR=/usr/share/vim/vim80
+			sudo make install
+			cd -
+		else
+			sudo apt-get install -y vim
+		fi
+	elif which yum > /dev/null
+	then
+		sudo yum install -y vim ctags automake gcc gcc-c++ kernel-devel cmake python-devel python3-devel git
+	fi
+
+}
+
 #echo install time
 function echo_install_time()
 {
@@ -85,5 +130,6 @@ function echo_install_time()
 
 get_start_time_and_dir_path
 check_network
-build_and_install_vim
+#build_and_install_vim
+build_vim_by_source
 echo_install_time
