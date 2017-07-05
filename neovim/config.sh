902808d0e22b407b7ca11b135321517101604809
nma1x <ningx.ma@intel.com>
2017-07-05 13:07:56 +0800

    [neovim] update v0.1.1

--- neovim/config.sh
+++ neovim/config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # ~/.config/nvim/init.vim
 cp nvim/ ~/.config/ -rf
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# install nvim plugin
 nvim +PlugInstall +qall
