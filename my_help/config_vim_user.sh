a57bcb1c113af52b9c497dc4f2afbf1084e86ff9
sky8336 <8336391@qq.com>
2019-11-06 18:32:42 +0800

    git push github 免输入账号和密码方法
    Signed-off-by: sky8336 <8336391@qq.com>

--- my_help/config_vim_user.sh
+++ my_help/config_vim_user.sh
@@ -2,3 +2,6 @@
 git config user.name sky8336 && git config user.email 8336391@qq.com
 git config user.name && git config user.email
 
+#git push github 免输入账号和密码方法
+#git config --global credential.helper store
+git config credential.helper store
