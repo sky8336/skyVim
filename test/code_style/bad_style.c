22164b9939b6cc8a284166c36655b76493ec166e
sky8336 <8336391@qq.com>
2019-11-22 09:48:10 +0800

    [test] add bad_style.c for test
    Signed-off-by: sky8336 <8336391@qq.com>

---
+++ test/code_style/bad_style.c
@@ -0,0 +1,42 @@
+/*
+ * a.c
+ *
+ * Copyright (C) 2018-2023 eric  <eric@company.com>. All Rights Reserved.
+ *
+ * History:
+ *    2019/11/22 - [eric] Created file
+ *
+ * Maintainer: eric <eric@email.com>
+ *    Created: 2019-11-22
+ * LastChange: 2019-11-22
+ *    Version: v0.0.01
+ *
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i=5;
+	int foo=1;
+    char * name = "hello";
+
+    if(i==5)
+	{
+        printf("%s\n",name);
+    }
+
+	switch(foo)
+	{
+		case 1:
+			a += 1;
+			break;
+
+		case 2:
+			{
+				a += 2;
+				break;
+			}
+	}
+
+    return 0;
+}
