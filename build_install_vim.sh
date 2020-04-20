02fa6cc2f80e66f923ffc180ebabf092ea1f93b1
sky8336 <8336391@qq.com>
2020-04-20 19:22:36 +0800

    build_install_vim.sh: update .bashrc_my after install vim

--- build_install_vim.sh
+++ build_install_vim.sh
@@ -10,10 +10,10 @@
 # Maintainer: you <your@email.com>
 #    Created: 2016-08-17
 # LastChange: 2020-04-20
-#    Version: v0.0.19
+#    Version: v0.0.20
 #
 
-source ./utils.sh
+source ./common.sh
 
 # global
 opt_dependent="0"
@@ -24,7 +24,8 @@ opt_install="4"
 opt_build_install="5"
 opt_clone_build_install="6"
 opt_update_build_install="7"
-update_bashrc_my="8"
+opt_update_bashrc_my="8"
+opt_max=$opt_update_bashrc_my
 
 # show_header specify content
 VERSION=0.0.17
@@ -54,7 +55,7 @@ examples=(
 		`basename $0` $opt_build_install - build and install
 		`basename $0` $opt_clone_build_install - clone, build and install
 		`basename $0` $opt_update_build_install - update, build and install
-		`basename $0` $update_bashrc_my - update .bashrc_my for vim built from source"
+		`basename $0` $opt_update_bashrc_my - update .bashrc_my for vim built from source"
 )
 
 #modify vim version here
@@ -73,47 +74,6 @@ newvim_c_vim="/usr/share/vim/$new_vim/syntax/c.vim"
 # APIs
 #
 
-#获取开始时间和路径
-function get_dir_path()
-{
-	vimcfig_bundle_dir_path=$(pwd)
-	#echo "dir_path: $vimcfig_bundle_dir_path"
-}
-
-#检查root权限
-function check_root_privileges()
-{
-	if [ $UID -eq 0 ]; then
-		echo -e "${FUNCNAME[0]}(): ${color_failed}>>> Error: Remove you root privileges!"
-		echo -e "Please input \"./`basename $0`\"${color_reset}"
-		exit
-	else
-		echo "${FUNCNAME[0]}(): You have normal privileges!"
-	fi
-}
-
-#shell脚本下载数据时，先检测网络的畅通性
-function check_network()
-{
-	#超时时间
-	timeout=10
-
-	#目标网站
-	target=www.baidu.com
-
-	#获取响应状态码
-	ret_code=`curl -I -s --connect-timeout $timeout $target -w %{http_code} | tail -n1`
-
-	if [ "x$ret_code" = "x200" ]; then
-		#网络畅通
-		pstatus green "connected" "network state"
-	else
-		#网络不畅通
-		pstatus red "the connection is lost" "network state"
-		error_exit "${color_failed}the connection is lost !  Please check your Internet connection."
-	fi
-}
-
 
 #函数名、运算符、括号等高亮
 function add_hilight_code_to_c_vim()
@@ -124,7 +84,7 @@ function add_hilight_code_to_c_vim()
 			echo "Found my_vim_highlight_config! $1 have been modified."
 		else
 			echo "Not found my_vim_highlight_config! Modify $1 now."
-			cat $vimcfig_bundle_dir_path/.self_mod/highlight_code.vim >> $1
+			cat $skyvim_path/.self_mod/highlight_code.vim >> $1
 		fi
 	else
 		echo "can not found $1"
@@ -164,24 +124,7 @@ build_vim_repo()
 {
 	cd $vim_source
 
-	if [[ $vim_version = "v8.0" ]]; then
-		blue_log "build $vim_version"
-
-		# vim8.0 config
-		./configure --with-features=huge \
-			--enable-multibyte \
-			--enable-rubyinterp \
-			--enable-pythoninterp \
-			--with-python-config-dir=/usr/lib/python2.7/config \
-			--enable-perlinterp \
-			--enable-luainterp \
-			--enable-gui=gtk2 --enable-cscope --prefix=/usr
-
-		local major=$(git log --graph --decorate --pretty=oneline --abbrev-commit --all | grep "origin/master" | awk -F 'patch' '{print $2}' | awk -F ':' '{print $1}' | awk -F '.' '{print $1}'| sed 's/^[ \t]*//g')
-		local minor=$(git log --graph --decorate --pretty=oneline --abbrev-commit --all | grep "origin/master" | awk -F 'patch' '{print $2}' | awk -F ':' '{print $1}' | awk -F '.' '{print $2}')
-
-		make VIMRUNTIMEDIR=/usr/share/vim/vim${major}${minor}
-	elif [[ $vim_version = "v8.1" || $vim_version = "v8.2" ]]; then
+	if [[ $vim_version = "v8.1" || $vim_version = "v8.2" ]]; then
 		blue_log "build $vim_version"
 		# vim8.1/vim8.2 config
 		./configure --with-features=huge --enable-multibyte --enable-rubyinterp \
@@ -217,25 +160,6 @@ build_vim_repo()
 	cd -
 }
 
-update_bashrc_my()
-{
-	if [[ $vim_version = "v8.1" ]];then
-		# add the following to ~/.bashrc_my, replace of alias vi=
-		#alias vi='/usr/local/vim/bin/vim'
-		if [ -f ~/.bashrc_my ]; then
-			sed -i "s%^alias vi=.*$%alias vi='/usr/local/vim/bin/vim'%g" ~/.bashrc_my
-			echo "alias vimdiff='/usr/local/vim/bin/vimdiff'" >> ~/.bashrc_my
-		else
-			cp ~/.bashrc ~/bashrc.bak
-			echo "alias vi='/usr/local/vim/bin/vim'" >> ~/.bashrc
-			echo "alias vimdiff='/usr/local/vim/bin/vimdiff'" >> ~/.bashrc
-		fi
-
-		cp ~/.gitconfig ~/.gitconfig.bak
-		sed -i "s%^.*editor.*$%\teditor = /usr/local/vim/bin/vim%g" ~/.gitconfig
-	fi
-}
-
 install_vim_after_built()
 {
 	cd $vim_source
@@ -264,17 +188,17 @@ update_build_install_vim()
 
 main()
 {
+	sudo echo
 	set_color
 	check_root_privileges
-	get_start_time
+	get_start_time_and_dir_path
 
-	get_dir_path
 	show_logo
 
 	if [ -z "$1" ]; then
 		show_header
 		show_usage
-		warning_log "`basename $0` [opt]: opt should be 0, 1, ..., 7"
+		warning_log "`basename $0` [opt]: opt should be 0, 1, ..., $opt_max"
 		exit
 	elif [ "$1" = $opt_dependent ]; then
 		blue_log "install dependent package"
@@ -300,7 +224,7 @@ main()
 	elif [ "$1" = $opt_update_build_install ]; then
 		blue_log "update, build and install"
 		update_build_install_vim "$@"
-	elif [ "$1" = $update_bashrc_my ]; then
+	elif [ "$1" = $opt_update_bashrc_my ]; then
 		blue_log "update .bashrc_my for vim built from source"
 		update_bashrc_my "$@"
 	else
