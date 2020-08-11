0deac3bf819a46df46d54631178e15986beaa3ba
sky8336 <8336391@qq.com>
2020-08-11 16:58:28 +0800

    test(code_style): 添加测试case

--- test/code_style/bad_style.c
+++ test/code_style/bad_style.c
@@ -8,8 +8,8 @@
  *
  * Maintainer: eric <eric@email.com>
  *    Created: 2019-11-22
- * LastChange: 2020-08-10
- *    Version: v0.0.2
+ * LastChange: 2020-08-11
+ *    Version: v0.0.3
  *
  */
 #include <stdio.h>
@@ -19,6 +19,7 @@ int main(int argc, char *argv[])
     int i=5;
 	int foo=1;
     char * name = "hello";
+	CMsgQueuePtr   message_ptr_;
 
     if(i==5)
 	{
