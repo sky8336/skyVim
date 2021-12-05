c9e7f478c6e20db51ef1cae02195f1127d153df2
maning <maning1x@163.com>
2021-12-05 10:47:20 +0800

    [neovim] update build script
    Signed-off-by: maning <maning1x@163.com>

--- neovim/build_install_neovim.sh
+++ neovim/build_install_neovim.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # verified on ubuntu 16.04
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
+#sudo add-apt-repository -y ppa:neovim-ppa/unstable
+
+# rm /etc/apt/sources.list.d/neovim-ppa-ubuntu-unstable-bionic.list.
+sudo add-apt-repository -y ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get -y install neovim
 
-# install vim-plug 
+# install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
---
+++ neovim/readme.md
@@ -0,0 +1,10 @@
+# readme.md
+- Maintainer: eric <eric@email.com>
+-    Created: 2021-12-05
+- LastChange: 2021-12-05
+-    Version: v0.0.01
+
+
+## nvim
+neovim 所有配置都放在nvim目录下
+nvim --> ~/.config/nvim
