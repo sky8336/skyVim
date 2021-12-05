7cc0deb9f0cd81c09cbfb8aaef033fdea89c6f52
maning <maning1x@163.com>
2021-12-05 12:26:35 +0800

    add script to clone skyVim-plug
    Signed-off-by: maning <maning1x@163.com>

---
+++ clone_plug_offline.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Filename: clone_plug_offline.sh
+#
+# Copyright (C) 2018-2023 Eric MA  <eric@company.com>. All Rights Reserved.
+#
+# History:
+#    2021/12/05 - [Eric MA] Created file
+#
+# Maintainer: Eric MA <eric@email.com>
+#    Created: 2021-12-05
+# LastChange: 2021-12-05
+#    Version: v0.0.01
+#
+
+# when you can not access github.com, you can not install plugin online
+
+git clone https://github.com/sky8336/skyVim-plug
+
+# tar -zxvf plugged.tar.gz
+#
+# vim user:
+#   copy skyVim-plug/plugged ~/.vim/
+
+
+# neovim user:
+#   copy skyVim-plug/plugged ~/.config/nvim/
